d5b82069181e190cca082f7bdcf8cd329e69e32c
Rik-Fox <richard.fox@warwick.ac.uk>
2022-11-21 14:31:47 +0000

    func_testing.py for visualising adaptive reward updates

--- func_testing.py
+++ func_testing.py
@@ -9,11 +9,31 @@ for i in range(1, n):
     coeffs = np.append(coeffs, i + 1)
 
 x = np.linspace(0.0, 1.0, num=1000)
-plt.figure(figsize=(12, 9))
+fig, axs = plt.subplots(2, 2, figsize=(12, 9))
 
-for i in coeffs:
-    y = np.power(x, i)
-    plt.plot(x, y, label=f"x^{i}")
+Y = []
+y_diff = []
+
+for k, i in enumerate(coeffs):
+    Y.append(np.power(x, i))
+    axs[0][0].plot(x, Y[k], label=f"x^{i}")
+
+for k in range(len(Y)):
+    if k < 5:
+        y_diff.append(Y[k] - Y[4])
+    else:
+        y_diff.append(Y[4] - Y[k])
+
+    axs[1][0].plot(x, y_diff[k], label=f"y=x^{i} - y=x")
+
+for k in range(n):
+    axs[0][1].plot(x, np.subtract(Y[4 - k], Y[4 + k]), label=f"x^{k+1} - x^1/{k+1}")
+    axs[1][1].plot(
+        x,
+        np.subtract(y_diff[4 - k], y_diff[4 + k]),
+        label=f"x^{coeffs[4-k]} - x^{coeffs[4+k]}",
+    )
+
 
 plt.legend()
 plt.show()
